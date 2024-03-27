ce06ce2fc3a555fadffb8f56f8557d88b5c0aca7
admin <010421aa@gmail.com>
2024-03-27 14:32:04 +0900

    롤페이지

---
+++ js/Basic_js_test.js
@@ -0,0 +1,16 @@
+var jb='hi';
+var a=1;
+var b;
+b=5;
+
+if(true){
+    let c='let 접근';
+    var c_1='var 접근'
+}
+
+console.log(c_1);
+let d=5;
+console.log(d);
+
+const e='상수1 접근';
+console.log(e);
